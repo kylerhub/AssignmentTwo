7e02f5f420bfb994de2982036d1c43371c2ac11a
Jennifer-Wei Lin <jennifer-wei.lin@griffithuni.edu.au>
2023-05-02 10:06:49 +1000

    Comprehensively test your App, including the model, your viewmodel, and the user interface. MyDoc Readme

--- AssignmentTwo/AssignmentTwoTests/AssignmentTwoTests.swift
+++ AssignmentTwo/AssignmentTwoTests/AssignmentTwoTests.swift
@@ -5,38 +5,117 @@
 //  Created by jennifer-wei lin on 14/3/2023.
 //
 import XCTest
-@testable import AssignmentOne
+import CoreData
+@testable import AssignmentTwo
+
 
 final class AssignmentOneTests: XCTestCase {
-
-    //.Function for testing the items struct
-    func testItems() throws {
-        let item1 = Items(item: "Milk", checkedStatus: "unchecked", newCheckedStatus: "")
-        let item2 = Items(item: "Cheese", checkedStatus: "checked", newCheckedStatus: "")
-        
-        XCTAssert(item1.item == "Milk")
-        XCTAssert(item1.checkedStatus == "unchecked")
-        
-        XCTAssert(item2.item == "Cheese")
-        XCTAssert(item2.checkedStatus == "checked")
+    
+    var ctx: NSManagedObjectContext!
+    var view: ListDetailView!
+    
+    func testGetImage() async throws {
+        let place = FavouritePlace(context: ctx)
+        place.strUrl = "https://picsum.photos/200"
+        let image = try await place.getImage()
+        XCTAssertNotNil(image)
     }
     
-    /// Function for testing Checklist struct
+    func testSaveData() {
+        let place = FavouritePlace(context: ctx)
+        place.place = "Test Place"
+        place.strUrl = "https://example.com/image.png"
+        saveData()
+        let fetchRequest = NSFetchRequest<FavouritePlace>(entityName: "FavouritePlace")
+        let places = try! ctx.fetch(fetchRequest)
+        XCTAssertEqual(places.count, 1)
+        XCTAssertEqual(places[0].place, "Test Place")
+        XCTAssertEqual(places[0].strUrl, "https://example.com/image.png")
+    }
+    
+    func testDeletePlace() {
+        let contentView = ContentView()
+        let initialCount = contentView.favouritePlaces.count
+        
+        // Delete the first place
+        let placeToDelete = contentView.favouritePlaces.first
+        let idxToDelete = IndexSet(integer: 0)
+        contentView.deletePlace(idxToDelete)
+        
+        // Check that the number of places has decreased by 1
+        XCTAssertEqual(contentView.favouritePlaces.count, initialCount - 1)
+        
+        // Check that the correct place was deleted
+        XCTAssertFalse(contentView.favouritePlaces.contains(placeToDelete!))
+    }
+    
+    func testAddNewPlace() {
+        let contentView = ContentView()
+        let initialCount = contentView.favouritePlaces.count
+        
+        // Add a new place
+        contentView.place = "New Place"
+        contentView.addNewPlace()
+        
+        // Check that the number of places has increased by 1
+        XCTAssertEqual(contentView.favouritePlaces.count, initialCount + 1)
+        
+        // Check that the new place was added with the correct name
+        let newPlace = contentView.favouritePlaces.last
+        XCTAssertEqual(newPlace?.place, "New Place")
+    }
+    
+    func testAddNewPlaceDetails() throws {
+        view.name = "Test"
+        view.url = "https://www.example.com/image.jpg"
+        view.latitude = "37.7749"
+        view.longitude = "-122.4194"
+        view.notes = "Test notes"
+        view.addNewPlaceDetails()
+        XCTAssertNotNil(view.favouritePlace.place)
+        XCTAssertEqual(view.favouritePlace.place, "Test")
+        XCTAssertNotNil(view.favouritePlace.strUrl)
+        XCTAssertEqual(view.favouritePlace.strUrl, "https://www.example.com/image.jpg")
+        let detailsFetchRequest: NSFetchRequest<Detail> = Detail.fetchRequest()
+        detailsFetchRequest.predicate = NSPredicate(format: "belongto == %@", view.favouritePlace)
+        let details = try ctx.fetch(detailsFetchRequest)
+        XCTAssertEqual(details.count, 1)
+        XCTAssertEqual(details.first?.latitude, "37.7749")
+        XCTAssertEqual(details.first?.longitude, "-122.4194")
+        XCTAssertEqual(details.first?.notes, "Test notes")
+    }
+    
+    func testFetchDetails() throws {
+        view.name = "Test"
+        view.url = "https://www.example.com/image.jpg"
+        view.latitude = "37.7749"
+        view.longitude = "-122.4194"
+        view.notes = "Test notes"
+        view.addNewPlaceDetails()
+        view.fetchDetails()
+        XCTAssertEqual(view.details?.count, 1)
+        XCTAssertEqual(view.details?.first?.latitude, "37.7749")
+        XCTAssertEqual(view.details?.first?.longitude, "-122.4194")
+        XCTAssertEqual(view.details?.first?.notes, "Test notes")
+    }
 
-    func testChecklist() throws {
-           let checklist = Checklist()
-           XCTAssertEqual(checklist.checklist, "New checklist")
-           XCTAssertEqual(checklist.items.count, 0)
-       }
+    func testDeleteDetails() throws {
+        view.name = "Test"
+        view.url = "https://www.example.com/image.jpg"
+        view.latitude = "37.7749"
+        view.longitude = "-122.4194"
+        view.notes = "Test notes"
+        view.addNewPlaceDetails()
+        view.fetchDetails()
+        XCTAssertEqual(view.details?.count, 1)
+        view.deleteDetails(IndexSet(integer: 0))
+        XCTAssertEqual(view.details?.count, 0)
+        let detailsFetchRequest: NSFetchRequest<Detail> = Detail.fetchRequest()
+        detailsFetchRequest.predicate = NSPredicate(format: "belongto == %@", view.favouritePlace)
+        let details = try ctx.fetch(detailsFetchRequest)
+        XCTAssertEqual(details.count, 0)
+    }
     
-    /// Function for testing getFile() function for JSON persistence
-    
-    func testGetFile() throws {
-            let url = getFile()
-            XCTAssertNotNil(url)
-            XCTAssertTrue(url!.absoluteString.contains("mychecklists.json"))
-        }
-    ///Function for testing DataModel struct
     func testDataModel() {
         var dataModel = DataModel()
         
@@ -62,3 +141,5 @@ final class AssignmentOneTests: XCTestCase {
     }
     
 }
+
+
--- AssignmentTwo/AssignmentTwoTests/MyDoc.docc/MyDoc.md
+++ AssignmentTwo/AssignmentTwoTests/MyDoc.docc/MyDoc.md
@@ -8,29 +8,48 @@ Checklist app
 
 The project overview...
 
-List of Interactive Checklists
+The purpose of this milestone to create an advanced Master/Detail app with persistent data using CoreData.  
 
-Master View that displays a list of checklist. 
+You need to create a SwiftUI app that called FavouritePlaces, that runs on iOS in the simulator on the lab computers.  
 
-Each entry in the list (item) needs to show the name of the checklist (title).   
-Embed your list of checklists in a Navigation View with a title (use a suitable name for your list, e.g. "Checklists"). 
-Each item needs to be embedded in a Navigation Link that allows the user to get to a view that shows the corresponding checklist (Detail View) by clicking (tapping) on the Master View list item.  
-There needs to be a [+] button that allows to add a new Checklist and an 
-Edit button that puts the list of checklists into edit mode, allowing checklists to be deleted.   
+The goal of the App is to keep a list of your favourite places, including detailed information and a picture.  For the first milestone, implement a Master/Detail user interface with persistent data.  Ensure that the Master View displays a list of (any number) of places.  
 
-The Detail View should be the same view that you used for for last week's milestone with the addition of the ability to interact with the checklist, i.e. tick and untick items, 
-as well as an Edit button that allows deleting items and 
-resetting the check status of all items (i.e. unchecking all items).  
-Ensure that there is an undo function that restores the status of the ticked-off items, in case the user hit the Reset button by accident. 
-The Detail View now also needs to contain a back button (typically displaying "Back" or the name of the list in your Master View) at the top left that takes the user back to the list of checklists Master View.  
+Each entry in the list (item) needs to show a thumbnail preview of the image of the place, the name of the place, and the location. 
+
+The Master View needs to be fully editable, i.e., you need to be able to add, remove, and edit elements.
+
+Embed your list in a Navigation View with an editable title.   
+Each item needs to be embedded in a Navigation Link that allows the user to get to a Detail View by clicking (tapping) on the list item.  
+The Detail View should show at least the image (or its URL when in editing mode), the name, the location, and notes.  
+All elements including the image URLs and notes need to be fully editable, i.e., you need to be able to add, remove, and edit elements.  
+The Detail View also needs to contain a back button (typically displaying the name of the list) at the top left that takes the user back to the list (Master View).
 
 ## Topics
 
+Classes
+class Detail
+class FavouritePlace
+Structures
+struct AssignmentOneApp
 struct Checklist
+Struct for checklists with all items
 struct ContentView
+The ContentView struct is the Master View for the checklists that navigates to the items of each checklist Importantly, all data now need to be persistent through JSON serialisation.
 struct DataModel
+Struct for all the checklists
 struct Items
+Struct for all items with their checkedStatus
 struct ListDetailView
+The ListDetailView struct is the Detail View for the items within each checklist
+struct PH
+struct RowView
+Variables
+let defaultImage: Image
+var downloadImages: [URL : Image]
+Functions
+func getFile() -> URL?
+Function for JSON for persistence
+func saveData()
 
 ### <!--@START_MENU_TOKEN@-->Group<!--@END_MENU_TOKEN@-->
 
